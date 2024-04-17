b7f7e45c2daf495b3485b8d2789e41dd229856a0
iHateFrontEnd <norush927@gmail.com>
2024-04-17 22:08:49 +0530

    Sensor code

--- main.ino
+++ main.ino
@@ -1,30 +1,33 @@
 int TP1 = A1;
 int TP2 = A2;
+
 float TP1_Value = 0;
 float TP2_Value = 0;
 float diff = 0;
-void setup() {
-  // put your setup code here, to run once:
-pinMode(TP1,INPUT);
-pinMode(TP2,INPUT);
-Serial.begin(9600);
 
+void setup() {
+  pinMode(TP1,INPUT);
+  pinMode(TP2,INPUT);
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   TP1_Value = analogRead(TP1);
   TP1_Value = TP1_Value*(5.0/1023.0);
+
   TP2_Value = analogRead(TP2);
   TP2_Value = TP2_Value*(5.0/1023.0);
+
   diff = TP1_Value - TP2_Value;
-Serial.print("TP1 ");
-Serial.print(TP1_Value);
 
-Serial.print("  TP2 ");
-Serial.print(TP2_Value);
+  Serial.print("TP1 ");
+  Serial.print(TP1_Value);
 
-Serial.print("  diff ");
-Serial.println(diff);
-delay(500);
+  Serial.print("  TP2 ");
+  Serial.print(TP2_Value);
+
+  Serial.print("  diff ");
+  Serial.println(diff);
+  delay(500);
 }
