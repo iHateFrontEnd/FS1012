524dacb0ece004f49b42cb0adbe720d6851a76cc
iHateFrontEnd <norush927@gmail.com>
2024-04-22 21:43:29 +0530

    switched to bokeh

--- main.py
+++ main.py
@@ -1,56 +1,35 @@
-from matplotlib import pyplot as plt
+from bokeh.plotting import figure 
+from bokeh.io import export_png
 import pandas as pd
 
-def main():
+def generate_plots(sheet_name, df):
+    TP1_list = df['TP1'].tolist()
+    TP2_list = df['TP2'].tolist()
+    time_list = df.iloc[:, 0].tolist()
+    diff_list = df['Diff'].tolist()
 
+    TP1_graph = figure(title="TP1 graph", x_axis_label='Time', y_axis_label='TP1')
+    TP1_graph.line(time_list, TP1_list, line_width=5)
+    export_png(TP1_graph, filename=f'graphs/TP1 {sheet_name}.png')
+
+    TP2_graph = figure(title="TP2 graph", x_axis_label='Time', y_axis_label='TP2')
+    TP2_graph.line(time_list, TP2_list, line_width=5)
+    export_png(TP2_graph, filename=f'graphs/TP2 {sheet_name}.png')
+
+    diff_graph = figure(title="Diff graph", x_axis_label='Time', y_axis_label='Diff')
+    diff_graph.line(time_list, diff_list, line_width=5)
+    export_png(diff_graph, filename=f'graphs/diff {sheet_name}.png')
+    
+    print(f'sheet {sheet_name} done!')
+
+def main():
     for sheets in range(1, 6):
-        sheet_name = f'run {sheets}'    
+        sheet_name = f'run-{sheets}'    
         
         df = pd.read_excel('run-data_raw.xlsx', sheet_name=sheet_name)
 
-        columns = ['Time', 'TP1', 'TP2', 'Diff']
+        generate_plots(sheet_name, df)
 
-        df = df[columns]
-
-        TP1_list = []
-        TP2_list = []
-        time_list = []
-        diff_list = []
-
-        for i in df.Time:
-            time_list.append(i)
-
-        for i in df.TP1:
-            TP1_list.append(i)
-        
-        for i in df.TP2:
-            TP2_list.append(i)
-        
-        for i in df.Diff:
-            diff_list.append(i)
-
-        plt.title(f'TP1 {sheet_name}')
-        plt.plot(time_list, TP1_list)
-        plt.xlabel('Time')
-        plt.ylabel('TP1')
-        plt.savefig(f'graphs/TP1 {sheet_name}.png')
-        plt.clf()
-
-        plt.title(f'TP2 {sheet_name}')
-        plt.plot(time_list, TP2_list)
-        plt.xlabel('Time')
-        plt.ylabel('TP2')
-        plt.savefig(f'graphs/TP2 {sheet_name}.png')
-        plt.clf()
-
-        plt.title(f'Diff {sheet_name}')
-        plt.plot(time_list, diff_list)
-        plt.xlabel('Time')
-        plt.ylabel('Diff')
-        plt.savefig(f'graphs/diff {sheet_name}.png')
-        plt.clf()
-
-        print(f'sheet {sheet_name} done!')
 
 if __name__ == '__main__':
     main()
